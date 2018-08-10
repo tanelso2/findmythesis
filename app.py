34e0e806e2140d17c0d7577e392ab017bc78ca43
Thomas Nelson <tanelso2@gmail.com>
2018-08-09 17:23:12 -0700

    Redirects to main page instead of displaying 404 page

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
+import werkzeug.exceptions
 
 from thesis import get_thesis_topic
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def thesis():
     return render_template('index.html', thesis=get_thesis_topic())
 
+
 @app.route('/test')
 def test():
-    return render_template('index.html', thesis='Testing: A survey of secret access & simplicity in an age of containers')
+    return render_template('index.html',
+                           thesis='Testing: A survey of secret access & simplicity in an age of containers')
+
+
+@app.errorhandler(werkzeug.exceptions.NotFound)
+def redirect_to_main(_):
+    return redirect(url_for('thesis'))
