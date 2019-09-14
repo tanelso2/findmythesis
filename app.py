b7590900218ce6134b3ce091291fb3637f62f1c5
Thomas Nelson <tanelso2@gmail.com>
2019-09-14 08:54:45 -0700

    Switch from flask to aiohttp

--- app.py
+++ app.py
@@ -1,22 +1,76 @@
-from flask import Flask, redirect, render_template, url_for
-import werkzeug.exceptions
+#from flask import Flask, redirect, render_template, url_for
+# import werkzeug.exceptions
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+import asyncio
 
-from thesis import get_thesis_topic
+from thesis import get_thesis_topic, fetch_loop
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def thesis():
-    return render_template('index.html', thesis=get_thesis_topic())
-
-
-@app.route('/test')
-def test():
-    return render_template('index.html',
-                           thesis='Testing: A survey of secret access & simplicity in an age of containers')
+#app = Flask(__name__)
 
 
-@app.errorhandler(werkzeug.exceptions.NotFound)
-def redirect_to_main(_):
-    return redirect(url_for('thesis'))
+@aiohttp_jinja2.template('index.html')
+async def handle(_):
+    return {'thesis': await get_thesis_topic()}
+
+@aiohttp_jinja2.template('index.html')
+async def test(_):
+    return {'thesis': "Testing: A survey of secret access & simplicity in an age of containers"}
+
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/test', test)])
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+
+
+loop = asyncio.get_event_loop()
+print("Setting up task")
+loop.create_task(fetch_loop())
+loop.set_debug(True)
+
+
+async def handle_404(_, __):
+    return web.HTTPFound('/')
+
+
+def error_pages(overrides):
+    async def middleware(app, handler):
+        async def middleware_handler(request):
+            try:
+                response = await handler(request)
+                override = overrides.get(response.status)
+                if override is None:
+                    return response
+                else:
+                    return await override(request, response)
+            except web.HTTPException as ex:
+                override = overrides.get(ex.status)
+                if override is None:
+                    raise
+                else:
+                    return await override(request, ex)
+        return middleware_handler
+    return middleware
+
+
+error_middleware = error_pages({404: handle_404})
+app.middlewares.append(error_middleware)
+
+# def thesis():
+#     return render_template('index.html', thesis=get_thesis_topic())
+#
+#
+# @app.route('/test')
+# def test():
+#     return render_template('index.html',
+#                            thesis='Testing: A survey of secret access & simplicity in an age of containers')
+
+
+# @app.errorhandler(werkzeug.exceptions.NotFound)
+# def redirect_to_main(_):
+#     return redirect(url_for('thesis'))
+
+if __name__ == '__main__':
+    web.run_app(app)
--- thesis.py
+++ thesis.py
@@ -1,29 +1,52 @@
 from cachetools.func import ttl_cache
 import gspread
+import gspread_asyncio
 from oauth2client.service_account import ServiceAccountCredentials
 import random
+import asyncio
 
-
-def get_gspread_client(access_key=None):
+def get_service_account_creds(access_key=None):
     if not access_key:
         access_key = 'secret.json'
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name(access_key, scope)
-    return gspread.authorize(creds)
+    return creds
 
 
-@ttl_cache(maxsize=1, ttl=300)
-def get_thesis_values(client=None):
+async def get_gspread_client(access_key=None):
+    agcm = gspread_asyncio.AsyncioGspreadClientManager(get_service_account_creds)
+    agc = await agcm.authorize()
+    return agc
+
+
+async def get_thesis_values(client=None):
     if not client:
-        client = get_gspread_client()
-    sheet = client.open('Table - Reseach Paper Topics')
-    worksheet = sheet.worksheet('Raw Data')
-    return [worksheet.col_values(i) for i in range(1, worksheet.col_count + 1)]
+        client = await get_gspread_client()
+    sheet = await client.open('Table - Reseach Paper Topics')
+    worksheet = await sheet.worksheet('Raw Data')
+    return [await worksheet.col_values(i) for i in range(1, worksheet.col_count + 1)]
 
 
-def get_thesis_topic():
-    values = get_thesis_values()
-    choices = [random.choice(x) for x in values]
+cached_values = None
+
+
+async def get_thesis_topic():
+    global cached_values
+    if not cached_values:
+        cached_values = await get_thesis_values()
+    choices = [random.choice(x) for x in cached_values]
     # Idea: maybe move this into the index.html template?
     return f'{choices[0]}: {choices[1]} {choices[2]} & {choices[3]} {choices[4]}'
+
+
+FETCH_PERIOD_SEC = 10
+
+
+async def fetch_loop():
+    global cached_values
+    while True:
+        print("Updating values")
+        cached_values = await get_thesis_values()
+        await asyncio.sleep(FETCH_PERIOD_SEC)
+
